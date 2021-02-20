480cebf866f40dad91b00b9cbb869c5f382a64ec
Chelle77322 <serenynadarra@gmail.com>
2021-02-20 19:36:47 +1030

    Appending the test and lib javascript files for each of the roles

---
+++ start.js
@@ -0,0 +1,9 @@
+const App = require("./lib/team");
+const inquirer = require("inquirer");
+
+
+// Initialize new App run
+const app = new App(); 
+
+// Start playing
+app.start();
--- tests/standard_employee.js
+++ tests/standard_employee.js
@@ -12,37 +12,37 @@ test("Setting name id via constructor", () => {
 });
 
 test("Setting id value via constructor", () => {
-  const testValue = 100;
-  const Emp = new standad_employee("Foo", testValue);
-  expect(Emp.id).toBe(testValue);
+  const value = 100;
+  const Emp = new standad_employee("Foo", value);
+  expect(Emp.id).toBe(value);
 });
 
 test("Setting email via constructor", () => {
-  const testValue = "j.smith@test.com.au";
-  const Emp = new Employee("Foo", 1, testValue);
-  expect(Emp.email).toBe(testValue);
+  const value = "j.smith@test.com.au";
+  const Emp = new Employee("Foo", 1, value);
+  expect(Emp.email).toBe(value);
 });
 
 test("Accessing name through function getName()", () => {
-  const testValue = "Joanne Smith";
-  const Emp = new standad_employee(testValue);
-  expect(Emp.getName()).toBe(testValue);
+  const value = "Joanne Smith";
+  const Emp = new standad_employee(value);
+  expect(Emp.getName()).toBe(value);
 });
 
 test("Accessing id through function getId()", () => {
-  const testValue = 100;
-  const Emp = new standard_employee("Foo", testValue);
-  expect(Emp.getId()).toBe(testValue);
+  const value = 100;
+  const Emp = new standard_employee("Foo", value);
+  expect(Emp.getId()).toBe(value);
 });
 
 test("Accessing email through function getEmail()", () => {
-  const testValue = "j.smith@test.com.au";
-  const Emp = new standard_employee("Foo", 1, testValue);
-  expect(Emp.getEmail()).toBe(testValue);
+  const value = "j.smith@test.com.au";
+  const Emp = new standard_employee("Foo", 1, value);
+  expect(Emp.getEmail()).toBe(value);
 });
 
 test("getRole() should return \"standard_employee\"", () => {
-  const testValue = "standard_employee";
+  const value = "standard_employee";
   const Emp = new standard_employee("Joanne Smith", 1, "j.smith@test.com.au");
-  expect(Emp.getRole()).toBe(testValue);
+  expect(Emp.getRole()).toBe(value);
 });
